c45e6dc412e146c578ad42799dc3bc8b37e56412
jie-lv <jie-lv@99bill.com>
2022-08-24 10:15:14 +0800

    use postgres

--- db/init.go
+++ db/init.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 
@@ -18,23 +20,14 @@ var cacheInstance *cache.Cache
 
 // Init 初始化数据库
 func Init() error {
-	var user, pwd, addr, dataBase string
-	user = os.Getenv("MYSQL_USERNAME")
-	pwd = os.Getenv("MYSQL_PASSWORD")
-	addr = os.Getenv("MYSQL_ADDRESS")
-	dataBase = os.Getenv("MYSQL_DATABASE")
-	if dataBase == "" {
-		dataBase = "wxcomponent"
-	}
-	tcp := "tcp"
-	source := "%s:%s@" + tcp + "(%s)/%s?readTimeout=1500ms&writeTimeout=1500ms&charset=utf8&loc=Local&&parseTime=true"
-	source = fmt.Sprintf(source, user, pwd, addr, dataBase)
-	log.Debug("start inits mysql with ::::: " + source)
+	var db *gorm.DB
+	var err error = nil
 
-	db, err := gorm.Open(mysql.Open(source), &gorm.Config{
-		NamingStrategy: schema.NamingStrategy{
-			SingularTable: true, // use singular table name, table for `User` would be `user` with this option enabled
-		}})
+	if os.Getenv("SQL_DRIVER") == "postgres" {
+		db, err = initpostgres()
+	} else {
+		db, err = initmysql()
+	}
 
 	if err != nil {
 		fmt.Println("DB Open error,err=", err.Error())
@@ -56,7 +49,7 @@ func Init() error {
 
 	dbInstance = db
 
-	fmt.Println("finish inits mysql with ", source)
+	fmt.Println("finish inits db")
 
 	checkTables()
 
@@ -64,6 +57,48 @@ func Init() error {
 	cacheInstance = cache.New(5*time.Minute, 10*time.Minute)
 
 	return nil
+}
+
+func initpostgres() (*gorm.DB, error) {
+	dsn := "host=10.13.128.30 user=postgresUser password=postgresPW dbname=postgresDB port=5455 sslmode=disable TimeZone=Asia/Shanghai"
+	return gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // use singular table name, table for `User` would be `user` with this option enabled
+		}})
+}
+
+func initmysql() (*gorm.DB, error) {
+
+	var user, pwd, addr, dataBase string
+	user = os.Getenv("MYSQL_USERNAME")
+	pwd = os.Getenv("MYSQL_PASSWORD")
+	addr = os.Getenv("MYSQL_ADDRESS")
+	dataBase = os.Getenv("MYSQL_DATABASE")
+	if dataBase == "" {
+		dataBase = "wxcomponent"
+	}
+	tcp := "tcp"
+	source := "%s:%s@" + tcp + "(%s)/%s?readTimeout=1500ms&writeTimeout=1500ms&charset=utf8&loc=Local&&parseTime=true"
+	source = fmt.Sprintf(source, user, pwd, addr, dataBase)
+	log.Debug("start inits mysql with ::::: " + source)
+
+	return gorm.Open(mysql.Open(source), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // use singular table name, table for `User` would be `user` with this option enabled
+		}})
+}
+
+func checkPGTables() {
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `wxcallback_component` (`id` INT UNSIGNED AUTO_INCREMENT, `receivetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `infotype` VARCHAR(64) NOT NULL DEFAULT '', `postbody` TEXT NOT NULL, PRIMARY KEY (`id`), INDEX(`receivetime`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `wxcallback_biz` (`id` INT UNSIGNED AUTO_INCREMENT, `receivetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `tousername` VARCHAR(64) NOT NULL DEFAULT '', `appid` VARCHAR(64) NOT NULL DEFAULT '', `msgtype` VARCHAR(64) NOT NULL DEFAULT '', `event` VARCHAR(64) NOT NULL DEFAULT '', `postbody` TEXT NOT NULL, PRIMARY KEY (`id`), INDEX(`receivetime`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `comm` (`key` VARCHAR(64) NOT NULL, `value` TEXT NOT NULL, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `updatetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`key`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `user` ( `id` INT NOT NULL AUTO_INCREMENT, `username` VARCHAR(32) NOT NULL, `password` VARCHAR(64) NOT NULL, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `updatetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`ID`), UNIQUE KEY `user_username_uindex` (`username`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `authorizers` ( `id` INT NOT NULL AUTO_INCREMENT, `appid` VARCHAR(32) NOT NULL, `apptype` INT NOT NULL DEFAULT 0, `servicetype` INT NOT NULL DEFAULT 0, `nickname` VARCHAR(32) NOT NULL NOT NULL DEFAULT '', `username` VARCHAR(32) NOT NULL NOT NULL DEFAULT '', `headimg` VARCHAR(256) NOT NULL DEFAULT '', `qrcodeurl` VARCHAR(256) NOT NULL DEFAULT '',`principalname` VARCHAR(64) NOT NULL DEFAULT '', `refreshtoken` VARCHAR(128) NOT NULL DEFAULT '', `funcinfo` VARCHAR(128) NOT NULL DEFAULT '', `verifyinfo` INT NOT NULL DEFAULT -1, `authtime` TIMESTAMP NOT NULL, `updatetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY(`appid`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `wxcallback_rules` (`id` INT UNSIGNED AUTO_INCREMENT, `name` VARCHAR(64) NOT NULL DEFAULT '', `infotype` VARCHAR(64) NOT NULL DEFAULT '', `msgtype` VARCHAR(64) NOT NULL DEFAULT '', `event` VARCHAR(64) NOT NULL DEFAULT '', `type` INT NOT NULL DEFAULT 0, `open` INT NOT NULL DEFAULT 0,  `info` TEXT NOT NULL, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `updatetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY(infotype, msgtype, event)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `wxtoken` (`id` INT UNSIGNED AUTO_INCREMENT, `type` INT NOT NULL DEFAULT 0, `appid` VARCHAR(128) NOT NULL DEFAULT '', `token` TEXT NOT NULL, `expiretime` TIMESTAMP NOT NULL, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `updatetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY `appid_uindex` (`appid`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `counter` (`id` INT UNSIGNED AUTO_INCREMENT, `key` VARCHAR(64) NOT NULL, `value` INT UNSIGNED, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `updatetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY(`key`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+	dbInstance.Exec("CREATE TABLE IF NOT EXISTS `counter` (`id` INT UNSIGNED AUTO_INCREMENT, `key` VARCHAR(64) NOT NULL, `value` INT UNSIGNED, `createtime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, `updatetime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY(`key`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;")
+
 }
 
 func checkTables() {
--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/rs/zerolog v1.26.0
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	gorm.io/driver/mysql v1.2.0
+	gorm.io/driver/postgres v1.2.0
 	gorm.io/gorm v1.22.3
 )
 
@@ -21,6 +22,14 @@ require (
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/leodido/go-urn v1.2.0 // indirect
@@ -29,8 +38,9 @@ require (
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/ugorji/go/codec v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
